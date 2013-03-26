295153d5fd1d30dc0cc35a2feb0a597456b22876
Gao Yingkai <popol1991@gmail.com>
2013-03-26 11:39:34 +0800

    command dispatcher

---
+++ internalcmd.c
@@ -0,0 +1,21 @@
+#include "def.h"
+
+void cd(char* dir) {
+	// implement cd here
+}
+
+void jobs() {
+	// implement jobs here
+}
+
+void history() {
+	// implement history here
+}
+
+void fg(int jid) {
+	// implement fg here
+}
+
+void bg(int jid) {
+	// implement bg here
+}
---
+++ internalcmd.h
@@ -0,0 +1,5 @@
+void cd(char* dir);
+void jobs();
+void history();
+void fg(int jid);
+void bg(int jid);
